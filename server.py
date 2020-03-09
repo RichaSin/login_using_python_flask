cda827b93ad7814a04275afb36a9da97c2a9167f
Shivam Seth <shivam.seth@radisys.com>
2020-03-09 11:09:40 +0530

    refactor with pep8 std

--- server.py
+++ server.py
@@ -4,8 +4,9 @@ import argparse
 parser = argparse.ArgumentParser(description="Server to provide backend for login application.")
 parser.add_argument("--ip", type=str, default="127.0.0.1", help="IP on which server will listen")
 parser.add_argument("--port", type=str, default="5000", help="POST on which server will listen")
-parser.add_argument("--protocol", type=str, default="HTTP", choices=["HTTP"], help="Protocol on which server will listen")
-parser.add_argument("--logLevel", type="str", default="INFO", choices=["INFO", "DEBUG"], help="log level")
+parser.add_argument("--protocol", type=str, default="HTTP", choices=["HTTP"],
+                    help="Protocol on which server will listen")
+#parser.add_argument("--logLevel", type=str, default="INFO", choices=["INFO", "DEBUG"], help="log level")
 
 args = parser.parse_args()
 print(args)
@@ -18,52 +19,55 @@ loginAddress = baseAddress + "login"
 
 userCollection = dict()
 userCollection['admin'] = 'admin'
-app = Flask(__name__,static_url_path='/static')
+app = Flask(__name__, static_url_path='/static')
+
+
 @app.route('/', methods=["POST", "GET"])
 def index():
-  return render_template("login.html", signupURL=signupAddress,  loginURL=loginAddress)
+    return render_template("login.html", signupURL=signupAddress, loginURL=loginAddress)
+
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
-  if request.method == "POST":
-    loginName = request.form['userName']
-    loginPassword = request.form['password']
-    print("request : {}".format(request))
-    print("userName : {}".format(loginName))
-    print("userPassword : {}".format(loginPassword))
-    if (loginName in userCollection.keys()):
-      if (loginPassword == userCollection.get(loginName)):
-        msgStr = "Welcome {}, You have successfully login using flask server".format(loginName)
-        return render_template("response.html", message=msgStr, backToURL = loginAddress,  buttonCaption = "Login")
-      else:
-        msgStr = "Hey {}, Please provide correct password".format(loginName)
-        return render_template("response.html", message = msgStr, backToURL = loginAddress, buttonCaption = "Login")
-    else:
-      msgStr =  "Hey {}, Kindly signup first".format(loginName)
-      return render_template("response.html", message=msgStr, backToURL = signupAddress,  buttonCaption = "Sign up")
+    if request.method == "POST":
+        login_name = request.form['userName']
+        login_password = request.form['password']
+        print("request : {}".format(request))
+        print("userName : {}".format(login_name))
+        print("userPassword : {}".format(login_password))
+        if login_name in userCollection.keys():
+            if login_password == userCollection.get(login_name):
+                msg_str: str = "Welcome {}, You have successfully login using flask server".format(login_name)
+                return render_template("response.html", message=msg_str, backToURL=loginAddress, buttonCaption="Login")
+            else:
+                msg_str: str = "Hey {}, Please provide correct password".format(login_name)
+                return render_template("response.html", message=msg_str, backToURL=loginAddress, buttonCaption="Login")
+        else:
+            msg_str: str = "Hey {}, Kindly signup first".format(login_name)
+            return render_template("response.html", message=msg_str, backToURL=signupAddress, buttonCaption="Sign up")
 
-  elif request.method == "GET":
-    return render_template("login.html", signupURL=signupAddress, loginURL=loginAddress)
+    elif request.method == "GET":
+        return render_template("login.html", signupURL=signupAddress, loginURL=loginAddress)
 
 
 @app.route('/signup', methods=["POST", "GET"])
 def signup():
-  if request.method == "POST":
-    userName = request.form['userName']
-    userPassword = request.form['password']
+    if request.method == "POST":
+        user_name = request.form['userName']
+        user_password = request.form['password']
 
-    if userName in  userCollection.keys():
-      msgStr = "OhOo sorry, {}, is already taken, Please try something else".format(userName)
-      return render_template("response.html", message=msgStr, backToURL=signupAddress,  buttonCaption = "Sign up")
-    else:
-      userCollection[userName] = userPassword
-      msgStr = "Thanks {}, for the registration, You can login now".format(userName)
-      return render_template("response.html", message=msgStr, backToURL=loginAddress,  buttonCaption = "Login")
+        if user_name in userCollection.keys():
+            msg_str: str = "OhOo sorry, {}, is already taken, Please try something else".format(user_name)
+            return render_template("response.html", message=msg_str, backToURL=signupAddress, buttonCaption="Sign up")
+        else:
+            userCollection[user_name] = user_password
+            msg_str: str = "Thanks {}, for the registration, You can login now".format(user_name)
+            return render_template("response.html", message=msg_str, backToURL=loginAddress, buttonCaption="Login")
 
-  elif request.method == "GET":
-    return render_template("register.html", signupURL=signupAddress, loginURL=loginAddress)
+    elif request.method == "GET":
+        return render_template("register.html", signupURL=signupAddress, loginURL=loginAddress)
 
 
-if __name__ == '__main__':
-  #application will run on 8080 port
-  app.run(host=serverIp, port=serverPort)
+if __name__ == '__main_s_':
+    # application will run on 8080 port
+    app.run(host=serverIp, port=serverPort)
